c3acef6690c2762acc2fd6be42fae8a3cdc8f938
KEMELLYN CRISTINA FALCONE <87138016+kasfalcone@users.noreply.github.com>
2022-03-08 21:01:20 -0300

    Add files via upload

---
+++ Aula 3/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Joseph";
+p1.Idade = 2;
+//p1.EnderecoPessoa = default; ou
+p1.EnderecoPessoa = new Endereco()
+{
+    Logradouro = "Rua 1",
+    Numero = 2777,
+    CEP = "19013150",
+    Cidade = "São Paulo",
+
+};
+
+WriteLine("Fim");
